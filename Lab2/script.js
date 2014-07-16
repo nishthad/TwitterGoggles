a36e445fb160e00c8c196148b96327dddb256b9f
nishthad <nishtha.h.dalal@gmail.com>
2014-07-16 17:16:29 -0400

    UI for timeline info
    + handlebars library
    + displays username, screen name, followers and status count

--- Lab2/script.js
+++ Lab2/script.js
@@ -6,19 +6,41 @@ function Timeline(userid){
 
 Timeline.prototype.getData = function(){
     var self = this;
-    $.ajax({
+    console.log("getting data...");
+    $.ajax({ 
         url: "functions.php",
         data: {
             call: "getTimeline",
             query: self.userid
         },
         success: function(data){
-        var pretty = JSON.stringify(JSON.parse(data),null,2);
-        $("#response").append(pretty);
+            console.log("got data");
+            
+            self.render(JSON.parse(data));
         }
     });
     
     
+    
+    
+}
+
+
+Timeline.prototype.render = function(raw_data){
+   // console.log("render: "+ raw_data[0]);
+    var source = $("#timeline_info").html();
+    var template = Handlebars.compile(source);
+    
+    var context = {
+        user_name: raw_data[0].user.name,
+        screen_name: raw_data[0].user.screen_name,
+        followers: raw_data[0].user.followers_count,
+        statuses: raw_data[0].user.statuses_count
+        
+    }   
+    var html = template(context);
+    
+   $("#response").append(html)
 }
 
 $(document).ready(function(){
